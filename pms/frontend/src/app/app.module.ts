71fd4a421e4bcd0dce0fe697bf4163d540142bd9
deaxparadox <nitish136900kushwaha@gmail.com>
2023-11-19 22:47:40 +0530

    tour-of-heroes : building

--- pms/frontend/src/app/app.module.ts
+++ pms/frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
